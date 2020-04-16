592720fc013fd804a51c13cbcd5864184ac8e2f3
Jhoss <kuro.rc10@gmail.com>
2020-04-16 10:43:19 -0500

    Creando metodos de la clase Empleado y su constructor

--- tema_01/Tema_01/Tema_01/Empleado.cs
+++ tema_01/Tema_01/Tema_01/Empleado.cs
@@ -12,5 +12,30 @@ namespace Tema_01
         public string nombre { get; set; }
         public string categoria { get; set; }
         public double basico { get; set; }
+
+        public double bonificacion()
+        {
+            double bon = 0;
+            switch(categoria)
+            {
+                case "A": bon = 50;break;
+                case "B": bon = 85; break;
+                case "C": bon = 135; break;
+                default: bon = 0;break;
+            }
+
+            return bon;
+        }
+        public double pago()
+        {
+            return basico + bonificacion();
+        }
+        public Empleado()
+        {
+            dni = "";
+            nombre = "";
+            categoria = "A";
+            basico = 950;
+        }
     }
 }
