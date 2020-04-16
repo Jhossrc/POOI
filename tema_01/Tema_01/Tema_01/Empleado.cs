590ea3947c9692905ad0f222677308018ad2b867
Jhoss <kuro.rc10@gmail.com>
2020-04-16 10:32:51 -0500

    Creando clase Empleado y sus atributos

---
+++ tema_01/Tema_01/Tema_01/Empleado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_01
+{
+    public class Empleado
+    {
+        public string dni { get; set; }
+        public string nombre { get; set; }
+        public string categoria { get; set; }
+        public double basico { get; set; }
+    }
+}
